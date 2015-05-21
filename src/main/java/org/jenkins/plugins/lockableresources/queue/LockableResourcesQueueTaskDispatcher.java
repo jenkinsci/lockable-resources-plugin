8d00bc766ffc19a39d75f7c5c73275166391224b
Jim Carrothers <jim.carrothers@sap.com>
2015-05-21 13:06:26 -0700

    add logging of RuntimeExceptions during canRun implementation
    It seems that otherwise Jenkins suppresses these errors and builds which
    encounter them never run.

--- src/main/java/org/jenkins/plugins/lockableresources/queue/LockableResourcesQueueTaskDispatcher.java
+++ src/main/java/org/jenkins/plugins/lockableresources/queue/LockableResourcesQueueTaskDispatcher.java
@@ -16,6 +16,7 @@ import hudson.model.queue.QueueTaskDispatcher;
 import hudson.model.queue.CauseOfBlockage;
 
 import java.util.Collection;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.jenkins.plugins.lockableresources.LockableResourcesManager;
@@ -29,53 +30,58 @@ public class LockableResourcesQueueTaskDispatcher extends QueueTaskDispatcher {
 
 	@Override
 	public CauseOfBlockage canRun(Queue.Item item) {
-
-		AbstractProject<?, ?> project = Utils.getProject(item);
-		if (project == null)
-			return null;
-
-		LockableResourcesStruct resources = Utils.requiredResources(project);
-		if (resources == null ||
-			(resources.required.isEmpty() && resources.label.isEmpty())) {
-			return null;
-		}
-
-		int resourceNumber;
 		try {
-			resourceNumber = Integer.parseInt(resources.requiredNumber);
-		} catch (NumberFormatException e) {
-			resourceNumber = 0;
+			AbstractProject<?, ?> project = Utils.getProject(item);
+			if (project == null)
+				return null;
+
+			LockableResourcesStruct resources = Utils.requiredResources(project);
+			if (resources == null ||
+				(resources.required.isEmpty() && resources.label.isEmpty())) {
+				return null;
+			}
+
+			int resourceNumber;
+			try {
+				resourceNumber = Integer.parseInt(resources.requiredNumber);
+			} catch (NumberFormatException e) {
+				resourceNumber = 0;
+			}
+
+			LOGGER.finest(project.getName() +
+				" trying to get resources with these details: " + resources);
+
+			if (resourceNumber > 0 || !resources.label.isEmpty()) {
+
+				Collection<LockableResource> selected = LockableResourcesManager.get().queue(
+						resources,
+						item.id,
+						project.getFullName(),
+						resourceNumber,
+						LOGGER);
+
+				if (selected != null) {
+					LOGGER.finest(project.getName() + " reserved resources " + selected);
+					return null;
+				} else {
+					LOGGER.finest(project.getName() + " waiting for resources");
+					return new BecauseResourcesLocked(resources);
+				}
+
+			} else {
+				if (LockableResourcesManager.get().queue(resources.required, item.id)) {
+					LOGGER.finest(project.getName() + " reserved resources " + resources.required);
+					return null;
+				} else {
+					LOGGER.finest(project.getName() + " waiting for resources "
+						+ resources.required);
+					return new BecauseResourcesLocked(resources);
+				}
+			}
 		}
-
-		LOGGER.finest(project.getName() +
-			" trying to get resources with these details: " + resources);
-
-		if (resourceNumber > 0 || !resources.label.isEmpty()) {
-
-			Collection<LockableResource> selected = LockableResourcesManager.get().queue(
-					resources,
-					item.id,
-					project.getFullName(),
-					resourceNumber,
-					LOGGER);
-
-			if (selected != null) {
-				LOGGER.finest(project.getName() + " reserved resources " + selected);
-				return null;
-			} else {
-				LOGGER.finest(project.getName() + " waiting for resources");
-				return new BecauseResourcesLocked(resources);
-			}
-
-		} else {
-			if (LockableResourcesManager.get().queue(resources.required, item.id)) {
-				LOGGER.finest(project.getName() + " reserved resources " + resources.required);
-				return null;
-			} else {
-				LOGGER.finest(project.getName() + " waiting for resources "
-					+ resources.required);
-				return new BecauseResourcesLocked(resources);
-			}
+		catch ( RuntimeException ex ) {
+			LOGGER.log(Level.SEVERE, "Unexpected exception!", ex);
+			throw ex;
 		}
 	}
 
