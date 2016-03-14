4dbb5685c29a30fecb31acfab557f565299fd86f
Antonio Muñiz <amuniz@cloudbees.com>
2016-03-14 19:19:56 +0100

    Fixing test

--- src/test/java/org/jenkins/plugins/lockableresources/LockStepTest.java
+++ src/test/java/org/jenkins/plugins/lockableresources/LockStepTest.java
@@ -5,49 +5,58 @@ import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.model.Statement;
+import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
+
+import static org.junit.Assert.*;
 
 public class LockStepTest {
 
 	@Rule
 	public RestartableJenkinsRule story = new RestartableJenkinsRule();
 
+	@ClassRule
+	public static BuildWatcher buildWatcher = new BuildWatcher();
+
 	@Test
 	public void lockOrder() {
 		story.addStep(new Statement() {
 			@Override
 			public void evaluate() throws Throwable {
+				defineResource("resource1");
 				WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
 				p.setDefinition(new CpsFlowDefinition(
-						"echo 'Start'\n" +
-						"semaphore 'wait'\n" +
 						"lock('resource1') {\n" +
 						"	semaphore 'wait-inside'\n" +
 						"}\n" +
 						"echo 'Finish'"
 				));
 				WorkflowRun b1 = p.scheduleBuild2(0).waitForStart();
-				SemaphoreStep.success("wait/1", null);
 				SemaphoreStep.waitForStart("wait-inside/1", b1);
 
 				WorkflowRun b2 = p.scheduleBuild2(0).waitForStart();
-				SemaphoreStep.waitForStart("wait/2", b2);
 				WorkflowRun b3 = p.scheduleBuild2(0).waitForStart();
-				SemaphoreStep.waitForStart("wait/3", b3);
+				// Both 2 and 3 are waiting for locking resource1
 
-				// Let's b3 reach the lock before
-				SemaphoreStep.success("wait/3", null);
-				SemaphoreStep.success("wait/2", null);
+				story.j.waitForMessage("Waiting for lock...", b2);
+				story.j.waitForMessage("Waiting for lock...", b3);
 
+				// Unlock resource1
 				SemaphoreStep.success("wait-inside/1", null);
+				story.j.waitForMessage("Lock released on resouce [resource1]", b1);
 
-				System.out.println(b3.getLog());
-				story.j.assertLogContains("acquired", b3);
+				story.j.assertLogContains("Lock acquired on [resource1]", b2);
 			}
 		});
+	}
+
+	private void defineResource(String r) {
+		LockableResourcesManager.get().getResources().add(new LockableResource(r));
+		LockableResourcesManager.get().save();
 	}
 
 }
