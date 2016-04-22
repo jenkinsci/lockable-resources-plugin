7f0a799b69ec46c0044edfc11ce907c359ffd4a1
Florian Hug <aeon512@gmail.com>
2016-04-22 22:15:08 +0200

    Extended Pipeline Lock-Step with Label and Quantity

---
+++ src/main/java/org/jenkins/plugins/lockableresources/queue/QueuedContextStruct.java
@@ -0,0 +1,71 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2016, Florian Hug. All rights reserved.             *
+ *                                                                     *
+ * This file is part of the Jenkins Lockable Resources Plugin and is   *
+ * published under the MIT license.                                    *
+ *                                                                     *
+ * See the "LICENSE.txt" file for more information.                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package org.jenkins.plugins.lockableresources.queue;
+
+import java.io.Serializable;
+
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkins.plugins.lockableresources.queue.LockableResourcesStruct;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+/* 
+ * This class is used to queue pipeline contexts 
+ * which shall be executed once the necessary
+ * resources are free'd.
+ */
+public class QueuedContextStruct implements Serializable {
+
+	/*
+	 * Reference to the pipeline step context.
+	 */
+	private StepContext context;
+	
+	/*
+	 * Reference to the resources required by the step context.
+	 */
+	private LockableResourcesStruct lockableResourcesStruct;
+	
+	/*
+	 * Description of the required resources used within logging messages.
+	 */
+	private String resourceDescription;
+
+	/*
+	 * Constructor for the QueuedContextStruct class.
+	 */
+	public QueuedContextStruct(StepContext context, LockableResourcesStruct lockableResourcesStruct, String resourceDescription) {
+		this.context = context;
+		this.lockableResourcesStruct = lockableResourcesStruct;
+		this.resourceDescription = resourceDescription;
+	}
+	
+	/*
+	 * Gets the pipeline step context.
+	 */
+	public StepContext getContext() {
+		return this.context;
+	}
+	
+	/*
+	 * Gets the required resources.
+	 */
+	public LockableResourcesStruct getResources() {
+		return this.lockableResourcesStruct;
+	}
+
+	/*
+	 * Gets the resource description for logging messages.
+	 */
+	public String getResourceDescription() {
+		return this.resourceDescription;
+	}
+	
+	private static final long serialVersionUID = 1L;
+}
