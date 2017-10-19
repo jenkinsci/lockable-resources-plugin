5e13e6ea12599ff1a99eba834bdd215cec690109
Sue Carter <scarter@ahl.com>
2017-10-19 16:59:09 +0100

    Log the QueuedContexts collection each time it changes. Add a panel to the LockableResources page on Jenkins UI to display the QueuedContexts

--- src/main/java/org/jenkins/plugins/lockableresources/queue/QueuedContextStruct.java
+++ src/main/java/org/jenkins/plugins/lockableresources/queue/QueuedContextStruct.java
@@ -11,10 +11,11 @@ package org.jenkins.plugins.lockableresources.queue;
 import java.io.Serializable;
 import java.util.List;
 
+import hudson.model.Run;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
-import org.jenkins.plugins.lockableresources.queue.LockableResourcesStruct;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
+import org.kohsuke.stapler.export.Exported;
 
 /*
  * This class is used to queue pipeline contexts
@@ -38,6 +39,10 @@ public class QueuedContextStruct implements Serializable {
 	 */
 	private String resourceDescription;
 
+	// build information in the requesting context. Useful for displaying on the ui and logging
+	private transient volatile Run<?, ?> build = null;
+	private transient volatile String buildExternalizableId = null;
+
 	/*
 	 * Constructor for the QueuedContextStruct class.
 	 */
@@ -54,6 +59,34 @@ public class QueuedContextStruct implements Serializable {
 		return this.context;
 	}
 
+	@Exported
+	public String getBuildExternalizableId() {
+		if (this.buildExternalizableId == null) {
+			// getting the externalizableId can fail for many reasons, set to null if it fails for some reason
+			try {
+				buildExternalizableId = this.context.get(Run.class).getExternalizableId();
+			} catch (Exception e) {
+				buildExternalizableId = null;
+			}
+		}
+		return this.buildExternalizableId;
+	}
+
+	public Run<?, ?> getBuild() {
+		if (build == null) {
+			build = Run.fromExternalizableId(getBuildExternalizableId());
+		}
+		return build;
+	}
+
+	@Exported
+	public String getBuildName() {
+		if (getBuild() != null)
+			return getBuild().getFullDisplayName();
+		else
+			return null;
+	}
+
 	/*
 	 * Gets the required resources.
 	 */
@@ -64,9 +97,15 @@ public class QueuedContextStruct implements Serializable {
 	/*
 	 * Gets the resource description for logging messages.
 	 */
+	@Exported
 	public String getResourceDescription() {
 		return this.resourceDescription;
 	}
 
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String toString() {
+		return "Build(" + getBuildExternalizableId() + ") Resource(" + resourceDescription + ")";
+	}
 }
