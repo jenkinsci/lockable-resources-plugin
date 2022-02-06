1d15c921e54f1f0e36fc823248aa79d2a695427f
Gaspard Petit <gpetit@eidosmontreal.com>
2022-02-06 16:56:43 -0500

    Removed unnecessary Copyright to 6WIND

--- src/main/java/org/jenkins/plugins/lockableresources/LockableResourceProperty.java
+++ src/main/java/org/jenkins/plugins/lockableresources/LockableResourceProperty.java
@@ -1,11 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright (c) 2013, 6WIND S.A. All rights reserved.                 *
- *                                                                     *
- * This file is part of the Jenkins Lockable Resources Plugin and is   *
- * published under the MIT license.                                    *
- *                                                                     *
- * See the "LICENSE.txt" file for more information.                    *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package org.jenkins.plugins.lockableresources;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
