3d829463ead65b6fee51b12ef34bb7a52264234f
Jan-Frederik Schmidt <janfschmidt@mailbox.org>
2023-05-02 10:01:31 +0200

    style: Remove commented out line

--- src/main/java/org/jenkins/plugins/lockableresources/LockStepExecution.java
+++ src/main/java/org/jenkins/plugins/lockableresources/LockStepExecution.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map.Entry;
-// import java.util.Set;
 import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
