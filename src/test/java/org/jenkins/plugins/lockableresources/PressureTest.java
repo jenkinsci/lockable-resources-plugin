73efea1269d40dafd31f36ae872e68adac08164a
Pokorny Martin <martin.pokorny@etm.at>
2023-08-10 18:43:52 +0200

    press

--- src/test/java/org/jenkins/plugins/lockableresources/PressureTest.java
+++ src/test/java/org/jenkins/plugins/lockableresources/PressureTest.java
@@ -32,13 +32,13 @@ public class PressureTest extends LockStepTestBase {
     pressure(10);
   }
 
-  @Test
-  @WithTimeout(600)
-  public void pressure20() throws Exception {
-    pressure(20);
-    System.setProperty(Constants.SYSTEM_PROPERTY_DISABLE_SAVE, "true");
-    pressure(20);
-  }
+  // @Test
+  // @WithTimeout(600)
+  // public void pressure20() throws Exception {
+  //   pressure(20);
+  //   System.setProperty(Constants.SYSTEM_PROPERTY_DISABLE_SAVE, "true");
+  //   pressure(20);
+  // }
 
   // @Test
   // @WithTimeout(600)
