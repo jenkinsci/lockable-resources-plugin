488d011a420f081ba66c9147a555c0ad455d6b9a
Pokorny Martin <martin.pokorny@etm.at>
2023-09-06 22:29:50 +0200

    dummy commit

--- src/test/java/org/jenkins/plugins/lockableresources/PressureTest.java
+++ src/test/java/org/jenkins/plugins/lockableresources/PressureTest.java
@@ -55,8 +55,8 @@ public class PressureTest extends LockStepTestBase {
       lrm.createResourceWithLabel("resourceA_" + Integer.toString(i), "label1 label2");
       lrm.createResourceWithLabel("resourceAA_" + Integer.toString(i), "label");
       lrm.createResourceWithLabel("resourceAAA_" + Integer.toString(i), "label1");
-      lrm.createResourceWithLabel("resourceAAAA_" + Integer.toString(i), "(=%/!(/)?$/ HH( RU))");
     }
+    lrm.createResourceWithLabel("resourceAAAA_" + Integer.toString(i), "(=%/!(/)?$/ HH( RU))");
 
     // define groovy script used by our test jobs
     String pipeCode = "";
