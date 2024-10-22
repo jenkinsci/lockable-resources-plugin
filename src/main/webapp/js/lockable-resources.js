c7d90c43b2a6002bfc1e178723517fc998560e43
Basil Crow <me@basilcrow.com>
2024-10-22 09:30:20 -0700

    Consistent formatting with the rest of this file

--- src/main/webapp/js/lockable-resources.js
+++ src/main/webapp/js/lockable-resources.js
@@ -146,16 +146,16 @@ jQuery(document).ready(function () {
     }
   });
 
-  document.querySelectorAll(".lockable-resources-action-button").forEach(function(button) {
-    button.addEventListener("click", function(event) {
+  document.querySelectorAll(".lockable-resources-action-button").forEach(function (button) {
+    button.addEventListener("click", function (event) {
       const target = event.target;
       const action = target.dataset.action;
       resource_action(target, action);
     });
   });
 
-  document.querySelectorAll(".lockable-resources-replace-note").forEach(function(anchor) {
-    anchor.addEventListener("click", function(event) {
+  document.querySelectorAll(".lockable-resources-replace-note").forEach(function (anchor) {
+    anchor.addEventListener("click", function (event) {
       event.preventDefault();
       replaceNote(event.target.dataset.resourceName);
     });
